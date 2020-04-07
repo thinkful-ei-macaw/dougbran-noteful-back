4af593adfb97c4727b86ef5229026f7522dd7967
Douglas Miner <curr3nas@gmail.com>
2020-04-07 16:40:43 -0500

    added router files

---
+++ src/folders/folders-router.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const FoldersService = require('./folders-service')
+
+const foldersRouter = express.Router()
+const jsonParser = express.json()
+
+foldersRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    FoldersService.getAllFolders(knexInstance)
+      .then((folders) => {
+        res.json(folders);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name } = req.body;
+    const newFolder = { name };
+
+    if (newFolder.name == null)
+      return res
+        .status(400)
+        .json({
+          error: { message: "Missing name in request body" },
+        })
+
+    FoldersService.insertFolder(req.app.get("db"), newFolder)
+      .then((folder) => {
+        res
+          .status(201)
+          .location(`/folders/${note.id}`)
+          .json(folder);
+      })
+      .catch(next);
+  })
+
+foldersRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    FoldersService.getById(
+      req.app.get('db'),
+      req.params.folder_id
+    )
+      .then(folder => {
+        if (!folder) {
+          return res.status(404).json({
+            error: { message: "folder doesn't exist" },
+          })
+        }
+        res.folder = folder
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(folder)
+  })
+  .delete((req, res, next) => {
+    FoldersService
+      .deleteFolder(req.app.get("db"), req.params.folder_id)
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = foldersRouter
---
+++ src/notes/notes-router.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const notesService = require('./notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    notesService.getAllNotes(knexInstance)
+      .then((notes) => {
+        res.json(notes);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name } = req.body;
+    const newNote = { name };
+
+    if (newNote.name == null)
+      return res
+        .status(400)
+        .json({
+          error: { message: "Missing name in request body" },
+        })
+
+    notesService.insertnote(req.app.get("db"), newNote)
+      .then((note) => {
+        res
+          .status(201)
+          .location(`/notes/${note.id}`)
+          .json(note);
+      })
+      .catch(next);
+  })
+
+notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    notesService.getById(
+      req.app.get('db'),
+      req.params.note_id
+    )
+      .then(note => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: "note doesn't exist" },
+          })
+        }
+        res.note = note
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(note)
+  })
+  .delete((req, res, next) => {
+    notesService
+      .deletenote(req.app.get("db"), req.params.note_id)
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = notesRouter
